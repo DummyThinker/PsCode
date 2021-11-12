afd213e29d0483e14e6e182c52767666194f93a1
NotImplementedLife <not.implemented.life@gmail.com>
2021-11-12 11:05:29 +0200

    test examples

--- js/examples.js
+++ js/examples.js
@@ -4,7 +4,7 @@ function fetchExamples() {
 	examples_url="https://github.com/DummyThinker/PsCode/tree/master/examples"
 	fetch(examples_url, {credentials:"include"})
 	  .then((response) => {
-		return response.text());
+		return response.text();
 	  })
 	  .then((html) => {
 		console.log(html);
